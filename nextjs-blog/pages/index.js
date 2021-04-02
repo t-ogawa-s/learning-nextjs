07d860b2580462a2098cf1c71be06fe72bc3e47c
Taiki Ogawa <ogawa_taiki@sinap.jp>
2021-04-02 17:57:48 +0900

    add: component layoutの追加

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image'
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+)
 
 export default function Home() {
   return (
